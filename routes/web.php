52c414b0f4508d11325678c27467e032e5f4c446
evgeniy <uhvatkin.e@gigwork.ru>
2025-10-14 22:22:58 +0300

    шаг 1

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test-rollbar', function () {
-    \Log::debug('Test debug message from route');
-    \Log::warning('Test warning message');
-    \Log::error('Test error message');
-
-    return 'Test messages sent to Rollbar! Check your Rollbar dashboard.';
-});
