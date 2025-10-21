33feb10879b06bd6e132b1e1e2e1580b7408e829
evgeniy <uhvatkin.e@gigwork.ru>
2025-10-21 16:56:02 +0300

    шаг 2

---
+++ resources/views/task_statuses/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+    <div class="grid col-span-full">
+        <h1 class="mb-5">Создать статус</h1>
+        <form class="w-50" method="POST" action="{{ route('task_statuses.index') }}"><input type="hidden" name="_token" value="{{ csrf_token() }}">
+            @csrf
+            <div class="flex flex-col">
+                <div>
+                    <label for="name">Имя</label>
+                </div>
+                <div class="mt-2">
+                        <input type="hidden" name="_method" id="_method" value="POST">
+                            <input class="rounded border-gray-300 w-1/3" type="text" name="name" id="name" value="{{ $taskStatus->name }}">
+                            @if ($errors->any())
+                                @foreach ($errors->all() as $error)
+                                    <div class="text-rose-600">{{ $error }}</div>
+                                @endforeach
+                            @endif
+                        </div>
+                        <div class="mt-2">
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Создать</button>
+                        </div>
+                    </div>
+                </form>
+@endsection
+
--- resources/views/task_statuses/index.blade.php
+++ resources/views/task_statuses/index.blade.php
@@ -1,21 +1,17 @@
-<html lang="ru">
+@extends('layouts.app')
+@section('content')
 
-@include('layouts.head')
-
-<body>
-<div id="app">
-    @include('layouts.header')
-    <section class="bg-white dark:bg-gray-900">
-        <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
-            <div class="grid col-span-full">
-                @include('layouts.flash')
-
-                <h1 class="mb-5">Статусы</h1>
-
+    <div class="grid col-span-full">
+        <div class="alert alert-success"
+             role="alert">
+            @include('flash::message')
+        </div>
+        <h1 class="mb-5">Статусы</h1>
                 <div>
                     @auth
                         <a href="task_statuses/create" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Создать статус            </a>
+                            Создать статус
+                        </a>
                     @endauth
                 </div>
 
@@ -52,5 +48,4 @@
             </div>
     </section>
 </div>
-</body>
-</html>
+@endsection
