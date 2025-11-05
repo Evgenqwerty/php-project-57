2474f1bebbe52cce48301a98d1545d3c2caf5c7f
evgeniy <uhvatkin.e@gigwork.ru>
2025-11-05 23:15:30 +0300

    new commit

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,12 +10,23 @@ class Task extends Model
         'name',
         'description',
         'status_id',
+        'creator_by_id', // ← ДОБАВЬТЕ ЭТО
         'assigned_by_id',
     ];
 
     public function creator()
     {
         return $this->belongsTo('App\Models\User', 'creator_by_id');
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(TaskStatus::class, 'status_id');
+    }
+
+    public function assignedTo()
+    {
+        return $this->belongsTo(User::class, 'assigned_by_id');
     }
 
     public function labels()
