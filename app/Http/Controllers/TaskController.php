599c04284453525a49f63a51d9810a439cdf0323
evgeniy <uhvatkin.e@gigwork.ru>
2025-11-05 23:39:46 +0300

    new commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -61,15 +61,21 @@ class TaskController extends Controller
             'name' => "required|unique:tasks",
             'description' => "max:1000",
             'status_id' => "required|string",
+            'assigned_to_id' => "nullable|string", // принимаем оба варианта
             'assigned_by_id' => "nullable|string",
             'labels' => "nullable|array"
-
         ]);
+
+        // Преобразуем assigned_to_id в assigned_by_id если нужно
+        if (isset($data['assigned_to_id']) && !isset($data['assigned_by_id'])) {
+            $data['assigned_by_id'] = $data['assigned_to_id'];
+        }
+
         $task = new Task();
         $task->fill($data);
         $task->creator_by_id = Auth::user()->id;
-
         $task->save();
+
 
         if (array_key_exists('labels', $data)) {
             $task->labels()->attach($data['labels']);
