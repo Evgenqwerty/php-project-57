ab657db20fd3a8ffe23a198ddc8cf7a29ab95b45
evgeniy <uhvatkin.e@gigwork.ru>
2025-11-11 00:03:30 +0300

    правки

--- resources/views/task_statuses/index.blade.php
+++ resources/views/task_statuses/index.blade.php
@@ -1,48 +1,45 @@
 @extends('layouts.app')
 @section('content')
-
     <div class="grid col-span-full">
         @include('flash::message')
         <h1 class="mb-5">{{ __('layout.task_statuses') }}</h1>
-                <div>
-                    @auth
-                        <a href="{{ route('task_statuses.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('layout.task_statuses_create') }}</a>
-                    @endauth
-                </div>
-                <table class="mt-4">
-                    <thead class="border-b-2 border-solid border-black text-left">
+            <div>
+            @auth
+            <a href="{{ route('task_statuses.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('layout.task_statuses_create') }}</a>
+            @endauth
+            </div>
+            <table class="mt-4">
+                <thead class="border-b-2 border-solid border-black text-left">
                     <tr>
                         <th>{{ __('layout.table_id') }}</th>
                         <th>{{ __('layout.table_name') }}</th>
                         <th>{{ __('layout.table_date_of_creation') }}</th>
                         @auth
-                            <th>{{ __('layout.table_actions') }}</th>
+                        <th>{{ __('layout.table_actions') }}</th>
                         @endauth
                     </tr>
-                    </thead>
-                    @foreach ($taskStatuses as $status)
-                        <tr class="border-b border-dashed text-left">
-                            <td>{{ $status->id }}</td>
-                            <td>{{ $status->name }}</td>
-                            <td>{{ $status->created_at->format('Y-m-d') }}</td>
-                            @auth
-                                <td>
-                                    <a data-confirm="{{ __('layout.table_delete_question') }}"
-                                       data-method="delete"
-                                       class="text-red-600 hover:text-red-900"
-                                       href="{{ route('task_statuses.destroy', $status) }}"
-                                       rel="nofollow">{{ __('layout.table_delete') }}</a>
-                                    @csrf
-                                    @method('DELETE')
-                                    <a class="text-blue-600 hover:text-blue-900"
-                                       href="{{ route('task_statuses.edit', $status) }}">{{ __('layout.table_edit') }}</a>
-                                </td>
-                            @endauth
-                        </tr>
+                </thead>
+                @foreach ($taskStatuses as $status)
+                <tr class="border-b border-dashed text-left">
+                    <td>{{ $status->id }}</td>
+                    <td>{{ $status->name }}</td>
+                    <td>{{ $status->created_at->format('Y-m-d') }}</td>
+                    @auth
+                    <td>
+                        <a data-confirm="{{ __('layout.table_delete_question') }}"
+                           data-method="delete"
+                           class="text-red-600 hover:text-red-900"
+                           href="{{ route('task_statuses.destroy', $status) }}"
+                           rel="nofollow">{{ __('layout.table_delete') }}</a>
+                        @csrf
+                        @method('DELETE')
+                        <a class="text-blue-600 hover:text-blue-900"
+                           href="{{ route('task_statuses.edit', $status) }}">{{ __('layout.table_edit') }}</a>
+                    </td>
+                    @endauth
+                </tr>
                 @endforeach
-            </div>
-    </section>
-</div>
+    </div>
 @endsection
 @section('pagination')
     {{ $taskStatuses->links() }}
