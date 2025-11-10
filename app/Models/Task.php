c56aef9e2f6a0b5a8055f9325bfd9a822bcde3de
evgeniy <uhvatkin.e@gigwork.ru>
2025-11-10 22:35:41 +0300

    правки

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,14 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
- * @property \App\Models\User $creator
- * @property \App\Models\User $assignedTo
- * @property \App\Models\TaskStatus $status
- * @property \Illuminate\Database\Eloquent\Collection<\App\Models\Label> $labels
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property int $status_id
+ * @property int $creator_by_id
+ * @property int $assigned_by_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ *
+ * @property User $creator
+ * @property User $assignedTo
+ * @property TaskStatus $status
+ * @property \Illuminate\Database\Eloquent\Collection<Label> $labels
  */
-
 class Task extends Model
 {
     protected $fillable = [
@@ -21,23 +31,23 @@ class Task extends Model
         'assigned_by_id',
     ];
 
-    public function creator()
+    public function creator(): BelongsTo
     {
-        return $this->belongsTo('App\Models\User', 'creator_by_id');
+        return $this->belongsTo(User::class, 'creator_by_id');
     }
 
-    public function status()
+    public function status(): BelongsTo
     {
         return $this->belongsTo(TaskStatus::class, 'status_id');
     }
 
-    public function assignedTo()
+    public function assignedTo(): BelongsTo
     {
         return $this->belongsTo(User::class, 'assigned_by_id');
     }
 
-    public function labels()
+    public function labels(): BelongsToMany
     {
-        return $this->belongsToMany(Label::class);
+        return $this->belongsToMany(Label::class, 'task_label');
     }
 }
